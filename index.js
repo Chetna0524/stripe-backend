80c7e2ba06af26296ea29b7640b1b5f34806e554
Chetna0524 <159872600+Chetna0524@users.noreply.github.com>
2024-03-15 12:25:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-const CLIENT_URL = "https://shopping-app-pfyj.onrender.com";
+const CLIENT_URL = "https://shopping-app-2.onrender.com";
 
 app.post("/create-checkout-session", async (req, res) => {
 	const line_items = req.body.cartItems.map((item) => {
